5e7d2bacd8606f8758cc08903131e53586761e16
shery <382254994@qq.com>
2016-09-28 22:18:05 +0800

    update config

--- bundle/bundle.js
+++ bundle/bundle.js
@@ -44,8 +44,16 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	(function webpackMissingModule() { throw new Error("Cannot find module \"./source/js/index.js\""); }());
+	module.exports = __webpack_require__(1);
 
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	document.write('Here from indesjs!');
 
 /***/ }
 /******/ ]);
