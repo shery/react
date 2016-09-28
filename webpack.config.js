8a46a7a7e0e2b3b2a14f54e26c18e80fb8662d2d
shery <382254994@qq.com>
2016-09-28 22:37:09 +0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 // 使用 HtmlWebpackPlugin，将 bundle 好的 <script> 插入到 body。${__dirname} 为 ES6 语法对应的 __dirname
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: `${__dirname}/index.html`,
-  filename: 'indexbundle.html',
-  inject: 'body',
-});
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+// const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+//   template: `${__dirname}/index.html`,
+//   filename: 'indexbundle.html',
+//   inject: 'body',
+// });
 
 module.exports = {
   entry: [
