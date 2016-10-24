25086ed08333ca9327f1be36b987f9345bfb9868
shery <382254994@qq.com>
2016-10-24 22:28:58 +0800

    update

--- source/js/index.js
+++ source/js/index.js
@@ -5,16 +5,3 @@ import Greeter from './module.js';
 import '../css/index.css';
 
 render(<Greeter />, document.getElementById('root'));
-
-//  注意元件開頭第一個字母都要大寫
-class MyComponent extends React.Component {
-    // render 是 Class based 元件唯一必須的方法（method）
-    render() {
-        return (
-            <div>Hello, World!</div>
-        );
-    }
-}
-
-// 將 <MyComponent /> 元件插入 id 為 app 的 DOM 元素中
-ReactDOM.render(<MyComponent/>, document.getElementById('app'));
