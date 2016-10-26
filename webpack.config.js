1fa423cfc87549c5a1176131aa9297d9491746cb
shery <382254994@qq.com>
2016-10-26 16:06:11 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,8 @@ module.exports = {
       {
           test: /\.css$/,
           loader: ExtractTextPlugin.extract(
-              'style-loader',
-              'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader'
+              'style-loader?sourceMap',
+              'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]!postcss-loader?sourceMap'
           ),
       },
     ],
