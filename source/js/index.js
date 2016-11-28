4e96af0567e2b13ab3210edcce26028299b31848
shery15 <382254994@qq.com>
2016-11-28 19:12:33 +0800

    todo

--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './greeter.js';
-import Todo from './todo.js';
+import TodoApp from './todo.js';
 
 
 import '../css/index.css';
 
 render(<Greeter name="React" />, document.getElementById('root'));
-render(<Todo />, document.getElementById('todo'));
+render(<TodoApp />, document.getElementById('todo'));
