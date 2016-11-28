a35e0ef4403ce04653cf19d804afa799a4eca0fc
shery15 <382254994@qq.com>
2016-11-28 15:22:42 +0800

    props

--- source/js/greeter.js
+++ source/js/greeter.js
@@ -3,15 +3,25 @@ import CSSModules from 'react-css-modules';
 import styles from '../css/greeter.css'
 
 class Greeter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
     render() {
         return (
             <div styleName='outer'>
                 <div styleName='inner'>
-                    <span styleName='context'>Hi there and React greetings!</span>
+                    <span styleName='context'>Hi {this.props.name}!</span>
                 </div>
             </div>
         );
     }
+}
+Greeter.propTypes = {
+    name: React.ProTypes.string,
+}
+Greeter.defaultProps = {
+    name: 'props'
 }
 
 export default CSSModules(Greeter, styles, { allowMultiple: true });
