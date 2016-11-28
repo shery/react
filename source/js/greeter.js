bc8b6dd657cb9b12e7cefe43a04a02ac36fcec39
shery15 <382254994@qq.com>
2016-11-28 17:12:48 +0800

    state

--- source/js/greeter.js
+++ source/js/greeter.js
@@ -5,13 +5,25 @@ import styles from '../css/greeter.css'
 class Greeter extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.tick = this.tick.bind(this);
+        this.state = {
+            secondsElapsed: 0,
+        }
+    }
+    tick () {
+        this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+    }
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
     render() {
         return (
             <div styleName='outer'>
                 <div styleName='inner'>
-                    <span styleName='context'>Hi {this.props.name}!</span>
+                    <span styleName='context'>Hi {this.props.name}! tick: {this.state.secondsElapsed}</span>
                 </div>
             </div>
         );
