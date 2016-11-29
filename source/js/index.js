15236911cc17b8fc4b176f700990f8fa74458ac3
shery15 <382254994@qq.com>
2016-11-29 15:17:24 +0800

    info

--- source/js/index.js
+++ source/js/index.js
@@ -3,6 +3,8 @@ import {render} from 'react-dom';
 import Greeter from './greeter.js';
 import TodoApp from './todo.js';
 import MarkdownEditor from './markdowneditor.js';
+import UserGithub from './info.js';
+
 
 
 import '../css/index.css';
@@ -10,3 +12,4 @@ import '../css/index.css';
 render(<Greeter name="React" />, document.getElementById('root'));
 render(<TodoApp />, document.getElementById('todo'));
 render(<MarkdownEditor />, document.getElementById('markdowneditor'));
+render(<UserGithub />, document.getElementById('info'));
---
+++ source/js/info.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+class UserGithub extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            githubUrl: '',
+            avatarUrl: '',
+        }
+    }
+    componentDidMount() {
+        $.get(this.props.source, (result) => {
+            console.log(result);
+            const data = result;
+            if (data) {
+                this.setState({
+                    username: data.name,
+                    githubUrl: data.html_url,
+                    avatarUrl: data.avatar_url
+                });
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h3>{this.state.username}</h3>
+                <img src={this.state.avatarUrl} />
+                <a href={this.state.githubUrl}>link</a>
+            </div>
+        );
+    }
+}
+
+export default UserGithub;
