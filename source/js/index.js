22a06aa559b76a81758baa1678a20bafc7134346
shery15 <382254994@qq.com>
2016-11-29 15:27:12 +0800

    update

--- source/js/index.js
+++ source/js/index.js
@@ -12,4 +12,4 @@ import '../css/index.css';
 render(<Greeter name="React" />, document.getElementById('root'));
 render(<TodoApp />, document.getElementById('todo'));
 render(<MarkdownEditor />, document.getElementById('markdowneditor'));
-render(<UserGithub source="https://api.github.com/users/torvalds" />, document.getElementById('info'));
+render(<UserGithub source="https://api.github.com/users/shery15" />, document.getElementById('info'));
