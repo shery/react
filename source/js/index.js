c330c5952b048fa969a121a8c220471b9196bec6
shery15 <382254994@qq.com>
2016-11-29 11:41:01 +0800

    editor

--- source/js/index.js
+++ source/js/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './greeter.js';
 import TodoApp from './todo.js';
+import MarkdownEditor from './markdowneditor.js';
 
 
 import '../css/index.css';
 
 render(<Greeter name="React" />, document.getElementById('root'));
 render(<TodoApp />, document.getElementById('todo'));
+render(<MarkdownEditor />, document.getElementById('markdowneditor'));
