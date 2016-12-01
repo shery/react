12ccc1b8d6b5ac3e957d87b835b07a6d38465012
shery15 <382254994@qq.com>
2016-12-01 20:20:00 +0800

    fix

--- source/js/MarkdownEditor.js
+++ source/js/MarkdownEditor.js
@@ -14,7 +14,7 @@ class MarkdownEditor extends Component {
   }
   rawMarkup() {
     const md = new Remarkable();
-    return { __html: md.render(this.state.value) };
+    return md.render(this.state.value);
   }
 
   render() {
