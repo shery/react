e45486d5e008c3dccd86c62ac9a4053181cb30ba
shery15 <382254994@qq.com>
2016-12-01 20:03:08 +0800

    update

--- source/js/index.js
+++ source/js/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Greeter from './Greeter.js';
 import Todo from './Todo.js';
-import MarkdownEditor from './MarkdownEditor.js';
+// import MarkdownEditor from './MarkdownEditor.js';
 import Info from './Info.js';
 
 import '../css/index.css';
 
 render(<Greeter name="React" />, document.getElementById('root'));
 render(<Todo />, document.getElementById('todo'));
-render(<MarkdownEditor />, document.getElementById('markdowneditor'));
+// render(<MarkdownEditor />, document.getElementById('markdowneditor'));
 render(<Info source="https://api.github.com/users/shery15" />, document.getElementById('info'));
