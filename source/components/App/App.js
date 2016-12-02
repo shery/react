ca877244e65c596e16d7302550a73e4abd7915d0
shery15 <382254994@qq.com>
2016-12-02 16:52:18 +0800

    UPDATE

--- source/components/App/App.js
+++ source/components/App/App.js
@@ -3,7 +3,7 @@ import { Link, IndexLink } from 'react-router';
 
 const App = (props) => (
   <div>
-    <h1>React Router Tutorial</h1>
+    <h1>React Router Test</h1>
     <ul>
       <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
       <li><Link to="/info" activeStyle={{ color: 'green' }}>Info</Link></li>
