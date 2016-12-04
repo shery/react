b725f1916abd26492ad9ec034c541ad2b0447724
shery15 <382254994@qq.com>
2016-12-04 15:43:23 +0800

    update

--- source/components/Todo/index.js
+++ source/components/Todo/index.js
@@ -1,1 +1,1 @@
-export { default } from './App';
+export { default } from './Todo.js';
--- source/index.js
+++ source/index.js
@@ -19,7 +19,3 @@ render(
     </Route>
   </Router>,
   document.getElementById('app'));
-// render(<Greeter name="React" />, document.getElementById('root'));
-// render(<Todo />, document.getElementById('todo'));
-// render(<MarkdownEditor />, document.getElementById('markdowneditor'));
-// render(<Info source="https://api.github.com/users/shery15" />, document.getElementById('info'));
