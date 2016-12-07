a6b869d7c4a152867e4c4b6eb5c8d678503cfc8c
shery15 <382254994@qq.com>
2016-12-08 00:13:04 +0800

    update

--- source/components/Font/Font.js
+++ source/components/Font/Font.js
@@ -16,18 +16,17 @@ class Font extends Component {
   constructor(props) {
     super(props);
     // this.tick = this.tick.bind(this);
-    this.onStart = this.onStart.bind(this);
     this.state = {
       fontFaces: [{ name: '楷书', id: 0 }, { name: '隶书', id: 1 }, { name: '行书', id: 2 }],
       secondsElapsed: 0
     };
   }
-  componentWillMount() {
-    let canvas=document.getElementById('myCanvas');
-    let ctx=canvas.getContext('2d');
-    ctx.fillStyle='#FF0000';
-    ctx.fillRect(0,0,80,100);
-  }
+  // componentWillMount() {
+  //   let canvas=document.getElementById('myCanvas');
+  //   let ctx=canvas.getContext('2d');
+  //   ctx.fillStyle='#FF0000';
+  //   ctx.fillRect(0,0,80,100);
+  // }
   // componentDidMount() {
   //   this.interval = setInterval(this.tick, 1000);
   // }
