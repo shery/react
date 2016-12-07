42f28fce8565ec13a634cc3f06a81198e6eb34cf
shery15 <382254994@qq.com>
2016-12-08 00:09:47 +0800

    update

--- source/components/Font/Font.js
+++ source/components/Font/Font.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+const Canvas = props => (
+  <canvas width="500px;" height="500px" />
+);
 const FontList = props => (
   <select>
     {
@@ -13,10 +16,17 @@ class Font extends Component {
   constructor(props) {
     super(props);
     // this.tick = this.tick.bind(this);
+    this.onStart = this.onStart.bind(this);
     this.state = {
       fontFaces: [{ name: '楷书', id: 0 }, { name: '隶书', id: 1 }, { name: '行书', id: 2 }],
       secondsElapsed: 0
     };
+  }
+  componentWillMount() {
+    let canvas=document.getElementById('myCanvas');
+    let ctx=canvas.getContext('2d');
+    ctx.fillStyle='#FF0000';
+    ctx.fillRect(0,0,80,100);
   }
   // componentDidMount() {
   //   this.interval = setInterval(this.tick, 1000);
@@ -31,6 +41,7 @@ class Font extends Component {
     return (
       <div>
         <FontList fontFaces={this.state.fontFaces} />
+        <Canvas />
       </div>
     );
   }
