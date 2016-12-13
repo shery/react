f2a5c2337d2728cb025a0b16f8da64e90863d3a5
shery15 <382254994@qq.com>
2016-12-13 20:10:13 +0800

    update

--- source/components/Font/Font.js
+++ source/components/Font/Font.js
@@ -49,7 +49,7 @@ class Font extends Component {
     img.onload = () => {
       oCtx.drawImage(img, 0, 0);
     };
-    img.src = 'https://cdn.pixabay.com/photo/2016/10/06/14/07/sunflowers-1719119_960_720.jpg';
+    img.src = 'https://d13yacurqjgara.cloudfront.net/users/6084/screenshots/3152231/lighthouse.png';
     oCtx.font = '30px KaiTi';
     const textLength = oCtx.measureText('你好啊你好，');
     oCtx.fillText('长歌行长歌行长歌行', 10, 50, textLength.width);
