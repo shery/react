21cefee60070009e3edefa519f4dcb4494dd9e32
shery15 <382254994@qq.com>
2016-12-15 21:29:58 +0800

    update

--- source/components/Font/Font.js
+++ source/components/Font/Font.js
@@ -49,6 +49,7 @@ class Font extends Component {
     img.onload = () => {
       oCtx.drawImage(img, 0, 0);
       oCtx.font = '30px KaiTi';
+      oCtx.direction = 'ltr';
       const textLength = oCtx.measureText('你好啊你好，');
       oCtx.fillText('长歌行长歌行长歌行', 10, 50, textLength.width);
     };
