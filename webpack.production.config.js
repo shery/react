c23ef08f35cfb9304f6d45d44e00ee296b558cda
shery15 <382254994@qq.com>
2017-01-02 12:54:07 +0800

    update

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -9,7 +9,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 // 使用 HtmlWebpackPlugin，将 bundle 好的 <script> 插入到 body。${__dirname} 为 ES6 语法对应的 __dirname
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 //   template: `${__dirname}/index.html`,
