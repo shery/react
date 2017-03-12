561de05deebc4b2bfb0c32cbb02843643d0ed50f
shery15 <382254994@qq.com>
2017-03-13 00:14:03 +0800

    add topics

--- source/components/App/App.js
+++ source/components/App/App.js
@@ -16,17 +16,53 @@ const App = () => (
     <div className="tabs">
       <ul>
         <li><Link to="/">HOME</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
         <li><Link to="/info">Info</Link></li>
         <li><Link to="/todo">Todo</Link></li>
         <li><Link to="/markdowneditor">MarkdownEditor</Link></li>
       </ul>
       <hr />
       <Route exact path="/" component={Greeter} />
+      <Route path="/topics" component={Topics} />
       <Route path="/todo" component={Todo} />
       <Route path="/info" component={Info} />
       <Route path="/markdowneditor" component={MarkdownEditor} />
     </div>
   </Router>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/component`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )} />
+  </div>
 );
 
 export default CSSModules(App, styles, { allowMultiple: true });
