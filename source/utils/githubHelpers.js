1cccf94a478f6929b3ad2d0e3afbe23404290cd0
shery15 <382254994@qq.com>
2017-03-14 20:52:41 +0800

    update

--- source/utils/githubHelpers.js
+++ source/utils/githubHelpers.js
@@ -1,6 +1,18 @@
-const helpers = {
-  getPlayersInfo() {
+import axios from 'axios';
 
+const id = 'Client_Id';
+const sec = 'Secret_Id';
+const param = ['?client_id=', id, '&client_secret=', sec].join('');
+
+function getUserInfo(username) {
+  return axios.get(['https://api.github.com/users/', username, param].join(''))
+}
+
+const helpers = {
+  getPlayersInfo(players) {
+    return axios.all(players.map(username => getUserInfo(username))
+    .then(info => info.map(user => user.data))
+    .catch(err => console.warn('Error in getPlayersInfo', err))
   }
 };
 
