a72b85ee8db6515a1559061f186137cfba4f2f69
shery15 <382254994@qq.com>
2017-03-16 13:16:45 +0800

    update

--- source/components/Main/Main.js
+++ source/components/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import TodoHeaderContainer from '../../containers/TodoHeaderContainer';
 import TodoListContainer from '../../containers/TodoListContainer';
 
