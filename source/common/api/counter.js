e4172d760bd0fe5e8599eddf3067cd6ddc78a66d
shery15 <382254994@qq.com>
2017-04-19 23:04:42 +0800

    update

--- source/common/api/counter.js
+++ source/common/api/counter.js
@@ -1,9 +1,12 @@
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min)) + min
-// }
-//
-// export function fetchCounter(callback) {
-//   setTimeout(() => {
-//     callback(getRandomInt(1, 100))
-//   }, 500)
-// }
+function sooner(year, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
+  const end = new Date(year, month - 1, day, hour, minute, second);
+  const now = new Date();
+  const duration = end.getTime() - now.getTime();
+  const rday = Math.floor(duration / 1000 / 60 / 60 / 24);
+  const rhour = Math.floor((duration / 1000 / 60 / 60) % 24);
+  const rmin = Math.floor((duration / 1000 / 60) % 60);
+  const rsecs = Math.floor((duration / 1000) % 60);
+  return { rday, rhour, rmin, rsecs };
+}
+
+export default sooner;
